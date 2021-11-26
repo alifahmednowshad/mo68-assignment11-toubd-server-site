f7a6ff9c23419ee547f8689053218dbcc5e37d8b
muhammadnowshad <muhammadnowshad2020@gmail.com>
2021-11-26 16:07:52 +0600

    last commit

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ async function run(){
           const user = req.body;
           const result = await usersCollection.insertOne(user)
           res.json(result);
-          console.log(result)
+        //   console.log(result)
         });
 
         app.put('/users', async (req, res) => {
